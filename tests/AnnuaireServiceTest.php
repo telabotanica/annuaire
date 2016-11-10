6db584bad762529fbb082d9d6bc380a20f7d18af
Mathias Chouet <mathias@tela-botanica.org>
2016-11-10 13:58:23 +0100

    Structure et config des tests

--- tests/AnnuaireServiceTest.php
+++ tests/AnnuaireServiceTest.php
@@ -1,8 +1,34 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class AnnuaireServiceTest extends PHPUnit_Framework_TestCase {
 
+	const CHEMIN_CONFIG = __DIR__ . "/config.json";
+	const CHEMIN_TESTS = __DIR__ . "/tests.json";
+
+	/** test URLs list */
+	protected $tests;
+
+	/** hosts config */
+	protected $config;
+
+	/** cURL handle */
+	protected $ch;
+
+	public function setUp(){
+		// config
+		$this->config = json_decode(file_get_contents(self::CHEMIN_CONFIG), true);
+		$this->tests = json_decode(file_get_contents(self::CHEMIN_TESTS), true);
+		// libcurl
+	}
+
+	public function tearDown(){}
+
+	public function testRetrocompat() {
+		var_dump($this->config);
+		var_dump($this->tests);
+	}
+
+	public function testAuth() {
+		// @TODO implémenter
+	}
+}
